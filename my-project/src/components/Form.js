52731674bffc3e9b71596fac1721a914f599e650
tatiana faramarzi <tfaramarzi@gmail.com>
2019-08-09 09:08:02 -0700

    Added form inputs and validation schema

--- my-project/src/components/Form.js
+++ my-project/src/components/Form.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
 
 const RegisterForm = () => {
 
     return (
-        <h2>Form will go here</h2>
+        <div className="form-container">
+            <Form>
+                <Field type="text" name="firstname" placeholder="your first name" />
+
+                <Field type="text" name="lastname" placeholder="your last name" />
+
+                <Field type="text" name="username" placeholder="enter a username" />
+
+                <Field type="password" name="password" placeholder="enter a password" />
+
+                <button type="submit">Submit</button>
+
+            </Form>
+        </div>
     )
 };
 
-export default RegisterForm;
+const FormikRegisterForm = withFormik({
+
+    mapPropsToValues({ firstname, lastname, username, password }) {
+        return {
+            firstname: firstname || '',
+            lastname: lastname || '',
+            username: username || '',
+            password: password || ''
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        firstname: Yup.string()
+            .required('please enter your first name.'),
+        lastname: Yup.string()
+            .required('please enter your last name.'),
+        username: Yup.string()
+            .min(4, 'your username must be at least 4 characters long.')
+            .required('please enter a username.'),
+        password: Yup.string()
+            .min(8, 'your password must be at least 8 characters long.')
+            .required('please enter a password.')
+    })
+
+    
+    //handleSubmit post to server function - pass in from App.js
+
+})(RegisterForm);
+
+export default FormikRegisterForm;
